4b0a921a9c3005649731f0a1f9be3c71870a0004
Sara Morita <65134819+sahmo1@users.noreply.github.com>
2022-09-20 11:22:37 -0700

    need to pass one test case in Account

--- utility-api/src/main/java/com/example/utilityapi/models/Account.java
+++ utility-api/src/main/java/com/example/utilityapi/models/Account.java
@@ -1,10 +1,15 @@
 package com.example.utilityapi.models;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Account {
 
+    @NotEmpty(message = "No duplicate usernames")
     private String username;
+    @NotEmpty(message = "Must enter a valid password")
+    @Size(min=0, max=10, message = "Password for newly created Account is capped at 10 characters.")
     private String password;
     private int id;
 
